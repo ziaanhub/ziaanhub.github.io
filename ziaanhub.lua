48c4adf17a8430b237e0d265b735b0949ac15e75
ziaandev <frmlnrmbee@gmail.com>
2025-11-01 00:55:46 +0700

    Update ziaanhub.lua

--- ziaanhub.lua
+++ ziaanhub.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/core/ziaancore.lua"))()
 
 local URL = Games[game.PlaceId]
 
